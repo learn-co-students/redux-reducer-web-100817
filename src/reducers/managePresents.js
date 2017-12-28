f4cf39cd24eae79ec42308f068182c99b4ee2db0
github username <github email address>
2017-12-28 15:10:22 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,10 @@
+// In managePresents.js, write a function called managePresents that takes in an action and the previous state as its argument.
+
 export function managePresents(state, action){
+  switch (action.type) {
+    case 'INCREASE':
+      return {numberOfPresents: state.numberOfPresents +1}
+    default:
+    return state;
+  }
 }
