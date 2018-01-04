db7e6e77fed7ad36c752e817cde5548365c51028
Liam Killion <liam.killion@gmail.com>
2018-01-04 18:14:24 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action) {
+  if (action.type === "ADD_FRIEND") {
+    let newFriends = Object.assign({}, state, {
+      friends: [...state.friends, action.friend]
+    });
+    return newFriends;
+  } else if (action.type === "REMOVE_FRIEND") {
+    let indexToRemove = state.friends.findIndex(
+      friend => friend.id === action.id
+    );
+    let newFriends = Object.assign({}, state, {
+      friends: [
+        ...state.friends.slice(0, indexToRemove),
+        ...state.friends.slice(indexToRemove + 1)
+      ]
+    });
+    return newFriends;
+  } else {
+    return { friends: state.friends };
+  }
 }
+
+// Error: Expected { friends: [ { name: 'Avi', hometown: 'NYC', id: 100 }, { name: 'Joe', hometown: 'Boston', id: 101 }, { name: 'Steven', hometown: 'Philadephia', id: 102 } ] }
+// to equal { friends: [ { name: 'Avi', hometown: 'NYC', id: 100 }, { name: 'Steven', hometown: 'Philadephia', id: 102 } ] }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,12 @@
-export function managePresents(state, action){
+export function managePresents(state, action) {
+  if (action.type === "INCREASE") {
+    return { numberOfPresents: state.numberOfPresents + 1 };
+  } else {
+    return { numberOfPresents: state.numberOfPresents };
+  }
 }
+// function changeState(state, action) {
+//   if (action.type === 'INCREASE_COUNT') {
+//     return {count: state.count + 1 }
+//   }
+// }
